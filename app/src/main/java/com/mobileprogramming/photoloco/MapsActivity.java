092c52b804d0376bc944e840a061f2f116b76727
Easton M Tuttle <etuttle1@uwyo.edu>
2019-03-05 17:42:11 -0700

    Current Location Marker and Click
    Marker appears at the current location and a click displays the image view.

--- app/src/main/java/com/mobileprogramming/photoloco/MapsActivity.java
+++ app/src/main/java/com/mobileprogramming/photoloco/MapsActivity.java
@@ -13,6 +13,7 @@
 package com.mobileprogramming.photoloco;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -21,6 +22,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
+import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -29,17 +31,20 @@ import android.widget.ImageView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
@@ -83,11 +88,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     // After camera app is used, save the photo taken.
@@ -96,14 +102,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ImageView imageView = findViewById(R.id.imageview);
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED)
-        {
-            getLocationPermission();
-        }
-        //mLastKnownLocation = fusedLocationClient.getLastLocation().getResult().getl;
-        LatLng currentLocation = new LatLng(fusedLocationClient.getLastLocation().getResult().getLatitude(), fusedLocationClient.getLastLocation().getResult().getLongitude());
-        //mMap.addMarker(new MarkerOptions().position(currentLocation).title("Marker at current location"));
+        //imageView.setImageBitmap(bitmap);
+        LatLng currentLocation = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+        Marker currentMarker = mMap.addMarker(new MarkerOptions().position(currentLocation).title("Marker at current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        currentMarker.setTag(bitmap);
     }
 
     @Override
@@ -123,8 +126,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         updateLocationUI();
     }
 
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Retrieve the data from the marker.
+        Bitmap image = (Bitmap) marker.getTag();
+        ImageView imageView = findViewById(R.id.imageview);
+        imageView.setImageBitmap(image);
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
+    }
+
     // Dispatch an intent to take a picture using the phone's native camera application.
     private void dispatchTakePictureIntent() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location == null) {
+                            return;
+                        }
+                        mLastKnownLocation = location;
+                    }});
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -166,8 +192,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.e("Exception: %s", e.getMessage());
         }
     }
-
-
-
-
 }
